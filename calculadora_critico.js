40d173441826914d86763c5dc289419c2d3b4ad0
Smashcg <94521507+Smashcg@users.noreply.github.com>
2021-11-19 07:21:43 -0300

    Add files via upload

---
+++ calculadora_critico.js
@@ -0,0 +1,7 @@
+function calcular(){
+    pcrit= Number(inputPCrit.value)
+    basecrit = Number(inputBasePCrit.value)
+    var chanceCritica =(((pcrit + 150)/150)*basecrit)
+    var critrestante = (((150*(100/basecrit))-150)-pcrit)
+    alert("Você tem " + (chanceCritica) + "% de Critar!\nFaltam " + Math.ceil(critrestante) + " P.crit para alcaçar os 100%!");
+}
